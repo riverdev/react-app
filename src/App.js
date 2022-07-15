5d6adce185f94e4beb0f4a5bf946af5b6395eaf3
riverdev <yariv.rivercybe@gmail.com>
2022-07-15 19:49:36 +0300

    chore(n): n

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer">
-          Yariv React Test 2022-07-15-1910
+          Yariv React Test 2022-07-15-1949
         </a>
       </header>
     </div>
