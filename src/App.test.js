392b6a7e922b70fa8acb4443d0beb62021182175
riverdev <yariv.rivercybe@gmail.com>
2022-07-16 11:04:39 +0300

    fix(tyuuuuu): dew  hg hg  ggrh r

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Yariv React Test 2022-07-16-1101/i);
+  const linkElement = screen.getByText(/Yariv React Test 2022-07-16-110x/i);
   expect(linkElement).toBeInTheDocument();
 });
